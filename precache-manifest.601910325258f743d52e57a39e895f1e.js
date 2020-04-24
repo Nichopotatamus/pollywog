8f1627472530827b3d2352188447bc8e561e993c
Nichopotatamus <Nichopotatamus@users.noreply.github.com>
2020-04-24 12:19:26 +0000

    deploy: ba61aef4a271eebe913fdd143e34e37e28d25f9d

---
+++ precache-manifest.601910325258f743d52e57a39e895f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba0ed768397c34549aa92e0742c47de",
+    "url": "/pollywog/index.html"
+  },
+  {
+    "revision": "9c852e498beb6eaafb4d",
+    "url": "/pollywog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67ee9bf64a7ef3f66ea4",
+    "url": "/pollywog/static/js/2.81938932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pollywog/static/js/2.81938932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c852e498beb6eaafb4d",
+    "url": "/pollywog/static/js/main.169e9b83.chunk.js"
+  },
+  {
+    "revision": "23454fb4239971ea7afa",
+    "url": "/pollywog/static/js/runtime-main.0f6a4c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pollywog/static/media/logo.5d5d9eef.svg"
+  }
+]);
