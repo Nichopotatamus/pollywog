4e505fe320343f6b5235f53a5dd5543844eff273
Nicholas Paulik <nicholas.paulik@schibsted.no>
2020-05-22 14:41:14 +0200

    Environment variable in nginx.conf

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React! Woop! Zoop!
+          Learn React! Woop! Boop!
         </a>
       </header>
     </div>
