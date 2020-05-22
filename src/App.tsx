7e9aa8f6e178c3d1e3be9a0c0fb0a0a91d8b358c
Nicholas Paulik <nicholas.paulik@schibsted.no>
2020-05-22 15:33:19 +0200

    Add app.json

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React! Woop! Boop! Zoop!
+          Learn React! Woop! Boop! Zoop! Moop!
         </a>
       </header>
     </div>
